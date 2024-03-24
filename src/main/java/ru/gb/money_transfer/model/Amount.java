12c61ccf44d0133cb41961b791861474e485ffc1
Leo <leonidamvrosov@yandex.ru>
2024-03-24 19:26:26 +0500

    added Amount

---
+++ src/main/java/ru/gb/money_transfer/model/Amount.java
@@ -0,0 +1,11 @@
+package ru.gb.money_transfer.model;
+
+import lombok.Data;
+
+@Data
+public class Amount {
+    // значение суммы перевода
+    private int value;
+    // валюта, в которой осуществляется перевод
+    private String currency;
+}
